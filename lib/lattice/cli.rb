3924cc9c7fe7a4581dc837319c5e3cc1572abe70
Nicholas Yang <ny585@nyu.edu>
2018-09-26 14:37:13 -0400

    generator works

--- lib/lattice/cli.rb
+++ lib/lattice/cli.rb
@@ -1,11 +1,26 @@
 require 'thor'
+require 'find'
 module Lattice
-  class CLI < Thor
-    desc "new NAME", "Creates new lattice project"
-    def new(name)
-      puts "Creating new project called #{name}"
+  class CLI < Thor::Group
+    include Thor::Actions
+
+    argument :name
+
+    def self.source_root
+      File.dirname(File.join(Dir.pwd, "lattice"))
+    end
+
+    def create_model_file
+      Find.find(File.join(Dir.pwd, 'templates')) do |file|
+        case File.extname(file)
+        when ".tt"
+          template(file, file.gsub("/templates", "").gsub(".tt", ""))
+        when /\.[A-Za-z]+/
+          template(file, file.gsub("/templates", ""))
+        end
+      end
+
     end
   end
 end
 
-Lattice::CLI.start(ARGV)
