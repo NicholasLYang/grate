889134905aaae9cad5ce2bdc9bf1abcaf9bb5566
Nicholas Yang <ny585@nyu.edu>
2018-11-18 00:17:50 -0500

    finished model generator

--- lib/grate/cli/generators.rb
+++ lib/grate/cli/generators.rb
@@ -1,5 +1,9 @@
-require_relative './new_model'
+require_relative './generators/base_generator'
+require_relative './generators/new_model'
+require_relative './generators/new_project'
 
-class Generate < Thor
-  register Grate::NewModel, 'model', 'model NAME', 'Creates a new model'
+module Grate
+  class Generate < Thor
+    register Grate::NewModel, 'model', 'model NAME', 'Creates a new model'
+  end
 end
--- lib/grate/cli/generators/new_model.rb
+++ lib/grate/cli/generators/new_model.rb
@@ -1,4 +1,3 @@
-require_relative './generator'
 require 'find'
 
 module Grate
